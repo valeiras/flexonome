123de89fdb6640c51b547e741dd8ebedd8c4d659
valeiras <d.r.valeiras@gmail.com>
2023-06-25 19:06:18 +0200

    Includes polyrhythm

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,60 +3,49 @@
 const MS_PER_MIN = 60000;
 const QUAVERS_PER_BAR = 4;
 
+// Main container
+const mainContainer = document.querySelector('#main-container');
+
 // Variables for buttons
 const playBtn = document.querySelector('#play-btn');
 const stopBtn = document.querySelector('#stop-btn');
-const addBtn = document.querySelector('#add-btn');
 
 // Variables for input
 const bpmInput = document.querySelector('#bpm');
 
-// Variables for time values
-let currInterval = 0;
-
-// Variables for iterating the array
-let currMeasureIdx = -1;
-let currNoteIdx = -1;
-
-// Variables for set interval and metronome status
-let metronomeInterval = null;
-
-let softSound = new Audio('./assets/sounds/softSound.mp3');
-let strongSound = new Audio('./assets/sounds/strongSound.mp3');
-
 let isPlaying = false;
 
-// Next note function
-function playNote() {
-
+// ---------------------- Main play note function -----------------------------
+function playNote(measureContainer) {
    // We unplay the last note and increase the indexes
-   if (currNoteIdx >= 0) {
-      toggleCurrentNote();
-      let nbNotes = getNbNotes(currMeasureIdx);
-      currNoteIdx = ++currNoteIdx % nbNotes;
+   if (measureContainer.currNoteIdx >= 0) {
+      toggleCurrentNote(measureContainer);
+      let nbNotes = getNbNotes(measureContainer, measureContainer.currMeasureIdx);
+      measureContainer.currNoteIdx = ++measureContainer.currNoteIdx % nbNotes;
    } else {
-      currNoteIdx = 0;
+      measureContainer.currNoteIdx = 0;
    }
 
-   if (currNoteIdx === 0) {
-      let nbMeasures = document.querySelectorAll('.measure').length;
-      currMeasureIdx = ++currMeasureIdx % nbMeasures;
+   if (measureContainer.currNoteIdx === 0) {
+      let nbMeasures = measureContainer.querySelectorAll('.measure').length;
+      measureContainer.currMeasureIdx = ++measureContainer.currMeasureIdx % nbMeasures;
    }
 
    // We play the current one
-   toggleCurrentNote();
-   if (getCurrNote().classList.contains('strong')) {
-      playSound(strongSound);
+   toggleCurrentNote(measureContainer);
+   if (getCurrentNote(measureContainer).classList.contains('strong')) {
+      playSound(measureContainer.strongSound);
    } else {
-      playSound(softSound);
+      playSound(measureContainer.softSound);
    }
 
-   if (currNoteIdx === 0) {
-      let newInterval = getCurrentInterval(currMeasureIdx);
-      if (newInterval !== currInterval) {
-         currInterval = newInterval;
-         window.clearInterval(metronomeInterval);
-         metronomeInterval = window.setInterval(playNote, currInterval);
+   // If we are reaching a new measure, we verify if the note value has changed
+   if (measureContainer.currNoteIdx === 0) {
+      let newInterval = getCurrentInterval(measureContainer);
+      if (newInterval !== measureContainer.currInterval) {
+         measureContainer.currInterval = newInterval;
+         window.clearInterval(measureContainer.metronomeInterval);
+         measureContainer.metronomeInterval = window.setInterval(playNote, measureContainer.currInterval, measureContainer);
       }
    }
 }
@@ -67,40 +56,60 @@ function playSound(sound) {
    sound.play();
 }
 
-function getNbNotes(measureIdx) {
-   let noteContainer = document.querySelectorAll('.note-container')[measureIdx];
+// ----------------------------------------- Getters ---------------------------------
+function getNbNotes(measureContainer, measureIdx) {
+   let noteContainer = measureContainer.querySelectorAll('.note-container')[measureIdx];
    return noteContainer.querySelectorAll('.note').length;
 }
 
-function getCurrNote() {
-   let currNoteContainer = document.querySelectorAll('.note-container')[currMeasureIdx];
-   return currNoteContainer.querySelectorAll('.note')[currNoteIdx];
+function getCurrentNote(measureContainer) {
+   let currNoteContainer = measureContainer.querySelectorAll('.note-container')[measureContainer.currMeasureIdx];
+   return currNoteContainer.querySelectorAll('.note')[measureContainer.currNoteIdx];
 }
 
-function toggleCurrentNote() {
-   getCurrNote().classList.toggle("active");
+function getCurrentInterval(measureContainer) {
+   let noteValues = measureContainer.querySelectorAll('.note-value');
+
+   // We verify that we are within bounds
+   let idx = measureContainer.currMeasureIdx < noteValues.length ? measureContainer.currMeasureIdx : 0;
+
+   let currNoteValue = noteValues[idx].value;
+   let bpm = bpmInput.value;
+
+   return (MS_PER_MIN / bpm) * (QUAVERS_PER_BAR / currNoteValue);
 }
 
-function removeRow(e) {
-   let rowToRemove = e.target.parentElement.parentElement;
-   let rowList = Array.prototype.slice.call(document.querySelectorAll('.row.measure'));
-   let idxToRemove = rowList.indexOf(rowToRemove);
+function toggleCurrentNote(measureContainer) {
+   getCurrentNote(measureContainer).classList.toggle("active");
+}
 
-   if (idxToRemove === currMeasureIdx) {
-      currNoteIdx = -1;
-      currMeasureIdx--;
-   } else if (idxToRemove < currMeasureIdx) {
-      currMeasureIdx--;
+function changeBeatsPerBar(e) {
+   let desiredNbNotes = e.target.value;
+
+   let currRow = e.target.parentElement.parentElement;
+   let noteContainer = currRow.querySelector('.note-container');
+   let currNbNotes = noteContainer.querySelectorAll('.note').length;
+   let notes = noteContainer.querySelectorAll('.note');
+
+   while (currNbNotes < desiredNbNotes) {
+      let newNote = document.createElement('div');
+      newNote.classList.add('note');
+      noteContainer.appendChild(newNote);
+      currNbNotes = noteContainer.querySelectorAll('.note').length
    }
-
-   rowToRemove.remove();
-   let nbMeasures = document.querySelectorAll('.measure').length;
-   if (nbMeasures === 1) {
-      let removeBtn = document.querySelector('.remove-btn');
-      removeBtn.disabled = true;
+   let currDeleteIdx = currNbNotes - 1;
+   while (currNbNotes > desiredNbNotes) {
+      // If we are removing the active note we reset the metronome
+      if (notes[currDeleteIdx].classList.contains('active')) {
+         currNoteIdx = -1;
+      }
+      notes[currDeleteIdx].remove();
+      currNbNotes = noteContainer.querySelectorAll('.note').length
+      currDeleteIdx--;
    }
 }
 
+// Auxiliary functions for the creation of a new row -----------------------------------------------
 function createNoteContainer(beatsPerBar) {
    let noteContainer = document.createElement('div');
    noteContainer.classList.add('note-container');
@@ -161,17 +170,17 @@ function createSignatureContainer(beatsPerBar, noteValue) {
    return signatureContainer;
 }
 
-function createRemoveButton() {
+function createRemoveButton(measureContainer) {
    let removeButton = document.createElement('button');
-   removeButton.classList.add('remove-btn');
+   removeButton.classList.add('remove-row-btn');
    removeButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
 
    removeButton.addEventListener("click", removeRow);
 
    // If this is the first (and thus only) measure, it cannot be removed
-   let nbMeasures = document.querySelectorAll('.measure').length;
+   let nbMeasures = measureContainer.querySelectorAll('.measure').length;
    if (nbMeasures > 1) {
-      let firstRemoveBtn = document.querySelector('.remove-btn');
+      let firstRemoveBtn = measureContainer.querySelector('.remove-row-btn');
       firstRemoveBtn.disabled = false;
    } else {
       removeButton.disabled = true;
@@ -180,40 +189,63 @@ function createRemoveButton() {
    return removeButton;
 }
 
-function changeBeatsPerBar(e) {
-   let desiredNbNotes = e.target.value;
+// Auxiliary functions for the creation of a new measure container --------------------------------------
+function createAddRowButton() {
+   let addRowButton = document.createElement('button');
+   addRowButton.classList.add('add-row-btn', 'add-btn', 'round');
+   addRowButton.innerHTML = '<i class="fa-solid fa-plus small"></i>';
+   return addRowButton;
+}
 
-   let currRow = e.target.parentElement.parentElement;
-   let noteContainer = currRow.querySelector('.note-container');
-   let currNbNotes = noteContainer.querySelectorAll('.note').length;
-   let notes = noteContainer.querySelectorAll('.note');
+function createAddColButton() {
+   let addColButton = document.createElement('button');
+   addColButton.classList.add('add-col-btn', 'add-btn', 'round');
+   addColButton.innerHTML = '<i class="fa-solid fa-plus small"></i>';
+   return addColButton;
+}
 
-   while (currNbNotes < desiredNbNotes) {
-      let newNote = document.createElement('div');
-      newNote.classList.add('note');
-      noteContainer.appendChild(newNote);
-      currNbNotes = noteContainer.querySelectorAll('.note').length
+function createRemoveColButton() {
+   let removeColButton = document.createElement('button');
+   removeColButton.classList.add('remove-col-btn', 'round');
+   removeColButton.innerHTML = '<i class="fa-solid fa-minus small"></i>';
+
+   let nbCols = mainContainer.querySelectorAll('.measure-container').length;
+   if (nbCols === 1) {
+      removeColButton.disabled = true;
    }
-   let currDeleteIdx = currNbNotes - 1;
-   while (currNbNotes > desiredNbNotes) {
-      // If we are removing the active note we reset the metronome
-      if (notes[currDeleteIdx].classList.contains('active')) {
-         currNoteIdx = -1;
-      }
-      notes[currDeleteIdx].remove();
-      currNbNotes = noteContainer.querySelectorAll('.note').length
-      currDeleteIdx--;
+
+   return removeColButton;
+}
+
+// Event listener for the MeasureContainer buttons ---------------------------------------
+function manageMeasureContainerButtons(evt) {
+   let btn = evt.target.parentElement;
+   if (btn.classList.contains('add-row-btn')) {
+      addRow(this);
+   } else if (btn.classList.contains('add-col-btn')) {
+      addMeasureContainer();
+      enableRemoveColButton();
+   } else if (btn.classList.contains('remove-col-btn')) {
+      removeMeasureContainer(this);
    }
 }
 
-function addNewRow() {
+function enableRemoveColButton() {
+   let firstRemoveBtn = document.querySelector('.remove-col-btn');
+   firstRemoveBtn.disabled = false;
+}
+
+function disableRemoveColButton() {
+   let firstRemoveBtn = document.querySelector('.remove-col-btn');
+   firstRemoveBtn.disabled = true;
+}
+
+function addRow(measureContainer) {
    let beatsPerBar = 4;
    let noteValue = 4;
-   let measureContainer = document.querySelector('#measure-container');
 
    let newRow = document.createElement('div');
-   newRow.classList.add('row');
-   newRow.classList.add('measure');
+   newRow.classList.add('row', 'measure');
    measureContainer.appendChild(newRow);
 
    let noteContainer = createNoteContainer(beatsPerBar);
@@ -222,27 +254,76 @@ function addNewRow() {
    let signatureContainer = createSignatureContainer(beatsPerBar, noteValue);
    newRow.appendChild(signatureContainer);
 
-   let removeButton = createRemoveButton();
+   let removeButton = createRemoveButton(measureContainer);
    newRow.appendChild(removeButton);
 }
 
-function getCurrentInterval(measureIdx) {
-   let noteValues = document.querySelectorAll('.note-value');
-   // We verify that we are within bounds
-   let idx = measureIdx < noteValues.length ? measureIdx : 0;
+function addMeasureContainer() {
+   let newMeasureContainer = document.createElement('div');
+   newMeasureContainer.classList.add('row-container', 'measure-container');
+   mainContainer.appendChild(newMeasureContainer);
 
-   let currNoteValue = noteValues[idx].value;
-   let bpm = bpmInput.value;
+   let addRowButton = createAddRowButton();
+   let addColButton = createAddColButton();
+   let removeColButton = createRemoveColButton();
 
-   return (MS_PER_MIN / bpm) * (QUAVERS_PER_BAR / currNoteValue);
+   newMeasureContainer.appendChild(addRowButton);
+   newMeasureContainer.appendChild(addColButton);
+   newMeasureContainer.appendChild(removeColButton);
+
+   addRow(newMeasureContainer);
+
+   newMeasureContainer.currMeasureIdx = -1;
+   newMeasureContainer.currNoteIdx = -1;
+   newMeasureContainer.currInterval = 0;
+   newMeasureContainer.isPlaying = false;
+   newMeasureContainer.metronomeInterval = null;
+   newMeasureContainer.softSound = new Audio('./assets/sounds/softSound.mp3');
+   newMeasureContainer.strongSound = new Audio('./assets/sounds/strongSound.mp3');
+
+   newMeasureContainer.addEventListener('click', manageMeasureContainerButtons);
+}
+
+function removeRow(evt) {
+   let rowToRemove = evt.target.parentElement.parentElement;
+   let measureContainer = rowToRemove.parentElement;
+   console.log(measureContainer);
+   let rowList = Array.prototype.slice.call(measureContainer.querySelectorAll('.measure'));
+   let idxToRemove = rowList.indexOf(rowToRemove);
+
+   if (idxToRemove === measureContainer.currMeasureIdx) {
+      measureContainer.currNoteIdx = -1;
+      measureContainer.currMeasureIdx--;
+   } else if (idxToRemove < measureContainer.currMeasureIdx) {
+      measureContainer.currMeasureIdx--;
+   }
+
+   rowToRemove.remove();
+   let nbMeasures = measureContainer.querySelectorAll('.measure').length;
+   if (nbMeasures === 1) {
+      let removeBtn = measureContainer.querySelector('.remove-row-btn');
+      removeBtn.disabled = true;
+   }
+}
+
+function removeMeasureContainer(measureContainer) {
+   measureContainer.remove();
+   let nbCols = mainContainer.querySelectorAll('.measure-container').length;
+   if (nbCols === 1) {
+      disableRemoveColButton();
+   }
 }
 
 playBtn.addEventListener('click', function () {
-   currMeasureIdx = -1;
-   currNoteIdx = -1;
-   currInterval = 0;
+   let measureContainerList = document.querySelectorAll('.measure-container');
+   measureContainerList.forEach(measureContainer => {
+      measureContainer.currMeasureIdx = -1;
+      measureContainer.currNoteIdx = -1;
+      measureContainer.currInterval = 0;
+      measureContainer.isPlaying = true;
+      playNote(measureContainer);
+   });
 
-   playNote();
    playBtn.disabled = true;
    stopBtn.disabled = false;
 
@@ -250,13 +331,18 @@ playBtn.addEventListener('click', function () {
 });
 
 stopBtn.addEventListener('click', function () {
-   window.clearInterval(metronomeInterval);
-   toggleCurrentNote();
+   let measureContainerList = document.querySelectorAll('.measure-container');
+   measureContainerList.forEach(measureContainer => {
+      if (measureContainer.isPlaying) {
+         window.clearInterval(measureContainer.metronomeInterval);
+         toggleCurrentNote(measureContainer);
+      }
+   });
+
    playBtn.disabled = false;
    stopBtn.disabled = true;
 
    isPlaying = false;
 })
 
-addBtn.addEventListener("click", addNewRow);
-document.addEventListener("DOMContentLoaded", addNewRow);
+document.addEventListener("DOMContentLoaded", addMeasureContainer);
